d0e10dfd80efb0bd43513129df583e2393528822
AtharvaPatange <164712914+AtharvaPatange@users.noreply.github.com>
2024-07-27 20:30:20 +0530

    final

--- AI/settings.py
+++ AI/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-gz-styp5w535!!j12x)bm*+4z4+t(7rvm9x(u3dsy_354e!2-a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS').split(',')
 
 
 
