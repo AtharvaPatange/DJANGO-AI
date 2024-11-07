c534a76c37dd62966e632a2b1e1c3123860053e4
AtharvaPatange <164712914+AtharvaPatange@users.noreply.github.com>
2024-11-07 12:57:32 +0530

    master CORS

--- AI/settings.py
+++ AI/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Recipe',
+    'corsheaders',
 
 ]
 
@@ -49,11 +50,19 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "https://django-ai-10.onrender.com",
+]
+CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'AI.urls'
 
